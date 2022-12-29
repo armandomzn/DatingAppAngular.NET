3ef872c100c125ab78cba0e45567fcbdc316c336
Armando <armandomm98@hotmail.com>
2022-12-28 21:41:08 -0600

    Skeleton in .NET

---
+++ API/Program.cs
@@ -0,0 +1,40 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//* Middleware
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during the migration");
+}
+
+app.Run();
